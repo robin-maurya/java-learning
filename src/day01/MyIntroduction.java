bb4ab8ba418241b702864115ea00adb9c41e8b97
Robin Maurya <robinmaurya01997@gmail.com>
2026-06-29 12:14:59 +0530

    Day 1: Added MyIntroduction program

---
+++ src/day01/MyIntroduction.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class MyIntroduction {
+    public static void main(String[] args) {
+        System.out.println("Hello Everyone");
+        System.out.println("My Name is Robin Maurya.");
+        System.out.println("I am learning Java.");
+        System.out.println("My goal is to become a Full Stack Developer.");
+    }
+}
