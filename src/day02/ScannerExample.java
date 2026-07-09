0fca7fb7c53eaf28aba2728bb4c0d06253a037c2
Robin Maurya <robinmaurya01997@gmail.com>
2026-07-10 02:31:17 +0530

    Day 2: Java Operators, Scanner and Type Casting

---
+++ src/day02/ScannerExample.java
@@ -0,0 +1,16 @@
+package day02;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+
+        String name = sc.nextLine();
+        System.out.println("Welcome " + name);
+
+        sc.close();
+    }
+}
